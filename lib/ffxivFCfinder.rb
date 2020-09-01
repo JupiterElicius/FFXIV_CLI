30d39cdc02fccea392651fe22f3a40aae3203200
JupiterElicius <bazalnick@gmail.com>
2020-09-01 13:56:06 -0700

    first commit

---
+++ lib/ffxivFCfinder.rb
@@ -0,0 +1,6 @@
+require "ffxivFCfinder/version"
+
+module FfxivFCfinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ffxivFCfinder/version.rb
@@ -0,0 +1,3 @@
+module FfxivFCfinder
+  VERSION = "0.1.0"
+end
